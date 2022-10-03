2c27fd08cf27a4db3e1805fab9fe68303f137c14
Sena <senatunc00@gmail.com>
2022-10-03 16:34:28 +0300

    first commit

---
+++ labyrinth3/Assets/camera_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour
+{
+    private float xRotation = 0f;
+    public float mouseSensitivity = 200f;
+    public Transform player;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //print(xRotation);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ labyrinth3/Assets/chr_movement2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class chr_movement2: MonoBehaviour
+{
+    public GameObject player;
+    public CharacterController controller;
+    public countDown count_d;
+    
+    public float speed;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "speed")
+        {
+            speed = 8.5f;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "hourglass")
+        {
+            count_d.timeStart +=60f;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "jump")
+        {
+            jumpHeight += 10f;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
